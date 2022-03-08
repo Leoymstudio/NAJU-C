773939b402beedd1dfc0989ac55dd7c90fc38dcc
李彦墨 <1784237811@qq.com>
2022-03-09 00:00:22 +0800

    第二节课

---
+++ test_3_8/test_3_8.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#define PI 3.1415926
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int main()
+//{
+//	char ch1 = 'a';
+//	char ch2 = 97;
+//	ch1 = ch1 + 1;
+//	ch2 = ch2 + 'b' - 'a';
+//	printf("%c\n%c\n", ch1,ch2);
+//
+//}
+
+//半角,,,,
+//全角，，，，
+
+//int main()
+//{
+	//printf("hello world!");
+	//1+2=?
+	//a+b=c
+	//int a, b, c;
+	//a = 1;       //赋值
+	//b = 2;
+	//c = a + b;
+	//printf("%d\n", c);
+	//printf("\a");
+	//printf("123456789\ba\n");
+	//printf("123456789\b\ra\n");
+	//printf("123\t456789");
+	//float r, c, s, v;
+	//r = 1;
+	//c = 2 * PI* r;
+	//s = PI* r*r;
+	//v = 4 / 3 * PI * r * r * r;
+	//printf("%f\n%f\n%f\n%f\n", r,c,s,v);
+//}
